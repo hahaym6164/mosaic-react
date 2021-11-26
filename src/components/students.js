f07bd830810203fa466759229ec022f584c8fe0e
unknown <616477730@qq.com>
2021-11-26 18:00:30 -0500

    update office NOV 26

--- src/components/students.js
+++ src/components/students.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
@@ -10,16 +10,7 @@ export default class students extends Component {
 
     // this.componentDidMount()
     this.state = {
-      studentList: [],
-      test: '',
-      testList: [{
-        test: 1,
-        test2: 2
-      }, {
-        test: 1,
-        test2: 2
-      }]
-
+      studentList: []
 
     }
   }
@@ -33,50 +24,50 @@ export default class students extends Component {
         console.log(err);
       })
   }
-  expandgrades(){
-    return
- 
+  expandGrades(input) {
+    this.setState(state => {
+      return state.studentList[input].showGrades = !state.studentList[input].showGrades
+      // console.log('expand', state.studentList[input].showGrades)
+    })
   }
   render() {
     let { studentList } = this.state;
-    let testList = this.state.testList;
 
     return (
       <div>
-      <div className="container">
-      
-      {studentList.map((i) => (
-        <div key={i.id}>
-          {/* use showGrages to switch the show grades */}
-          {i.showGrades=true}
+        <div className="container">
 
-          {console.log(i,'see show')}
-                 <h1>{i.firstName} {i.lastName}</h1>
-        <div className="student"  >
+          {studentList.map((i, index) => (
+            <div key={i.id}>
+              {/* use showGrages to switch the show grades */}
+              {i.showGrades = true}
 
-          <div>  
-          <img className="avatar" src={i.pic}></img></div>
-        <div className="student-info"> 
-        <ul className="list-info">
-        <li><span className={i.showGrades+ " title"}>City: </span>{i.city}</li>
-          <li><span className="title">Company: </span>{i.company}</li>
-          <li><span className="title">Cmail: </span>{i.email}</li>
-          <li><span className="title">Skill:</span> {i.skill}</li>
-        </ul>
+              {/* {console.log(studentList[index], i)} */}
+              <h1>{i.firstName} {i.lastName}</h1>
+              <div className="student"  >
 
-        <ul className="grades" id={i.id}>
-            {i.grades.map((grade,index) => (
-              <li key={index} className="grade">Grade:{grade}</li>
-            ))}
-          </ul>
-         </div>
-         <button className="expand-btn" onClick={this.expandgrades()}><i className="fas fa-plus"></i></button>
+                <div>
+                  <img className="avatar" src={i.pic}></img></div>
+                <div className="student-info">
+                  <ul className="list-info">
+                    <li><span className="title">City: </span>{i.city}</li>
+                    <li><span className="title">Company: </span>{i.company}</li>
+                    <li><span className="title">Email: </span>{i.email}</li>
+                    <li><span className="title">Skill:</span> {i.skill}</li>
+                  </ul>
 
+                  <ul className={(this.state.studentList[index].showGrades ? " " : "hidden") + " grades"}>
+                    {i.grades.map((grade, index) => (
+                      <li key={index} className="grade">Grade:{grade}</li>
+                    ))}
+                  </ul>
+                </div>
+                <button className="expand-btn" onClick={this.expandGrades.bind(this, index)}><i className="fas fa-plus"></i></button>
+
+              </div>
+            </div>
+          ))}
         </div>
-        </div>
-      ))}
-      {console.log(studentList)}
-      </div>
       </div>
     );
   }
