1c1c0b7b077daaa06cb76eda1bebb26f8bb7ea6b
unknown <616477730@qq.com>
2021-11-29 18:01:39 -0500

    update NOV29

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Students from './components/students';
-import Searchbar from './components/searchbar';
 const App = () => {
   return (
     <div className="App">
       <Students />
-      
+
     </div>
   );
 }
--- src/components/students.js
+++ src/components/students.js
@@ -1,41 +1,108 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import Searchbar from "./searchbar";
+import './style.css'
 const url = "https://www.hatchways.io/api/assessment/students"
 
 export default class students extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      studentList: []
+      studentList: [],
+      allStudentList: [],
+      searchByName: '',
+      searchByTag: ''
     }
-    
+
+  }
+  update() {
+    let update = this.state.searchByName || this.state.searchByTag ?
+      this.state.allStudentList.filter(i => {
+        let name = i.firstName + ' ' + i.lastName
+        return name.toLowerCase().includes(this.state.searchByName.toLowerCase())
+      }).filter(i => {
+        if (this.state.searchByTag) {
+          return i.tags ? i.tags.find(tag => tag.includes(this.state.searchByTag)) : false
+        } else {
+          return true
+        }
+      }) : this.state.allStudentList
+    this.setState({
+      studentList: update
+    })
+    console.log(update, 'update', this.state.searchByName)
   }
   componentDidMount() {
+
     axios
       .get(url)
       .then(res => {
-        this.setState({ studentList: res.data.students })
+        // if user enter search term, filter the student list with it
+
+        this.setState({
+          studentList: res.data.students,
+          allStudentList: res.data.students
+
+        })
       })
       .catch((err) => {
         console.log(err);
       })
   }
+  // filters for user input, NAME and TAG
+  filterByName(e) {
+    this.setState({
+      searchByName: e.target.value
+    }, () => this.update())
+
+  }
+  filterByTag(e) {
+    this.setState({
+      searchByTag: e.target.value
+    }, () => this.update())
+
+  }
   expandGrades(input) {
     let update = this.state.studentList
     update[input].showGrades = !update[input].showGrades
-    
+
     this.setState({
-      studentList : update
+      studentList: update
     })
   }
+  addTags(index, e) {
+
+    let value = e.target.value
+    if (e.key === "Enter") {
+      let update = this.state.studentList
+      if (update[index].tags) {
+        if (!(update[index].tags.indexOf(value) > -1)) {
+          update[index].tags.push(value)
+        } else {
+          alert(`This student already have this tag`)
+        }
+      } else {
+        update[index].tags = [value]
+      }
+
+      this.setState({
+        studentList: update,
+        allStudentList: update
+      })
+    }
+
+  }
   render() {
+
     let { studentList } = this.state;
     return (
       <div>
         <div className="container">
-              <Searchbar/>
+          <input className="search-bar" onKeyUp={this.filterByName.bind(this)} type="text" placeholder="Search by name" />
+          <input className="search-bar" onKeyUp={this.filterByTag.bind(this)} type="text" placeholder="Search by tag" />
+
+          {studentList.length > 0 ? '' : <div className="no-student">No student shown based on your search</div>}
+
           {studentList.map((i, index) => (
             <div key={i.id}>
 
@@ -51,15 +118,25 @@ export default class students extends Component {
                     <p><span className="title">Company: </span>{i.company}</p>
                     <p><span className="title">Email: </span>{i.email}</p>
                     <p><span className="title">Skill:</span> {i.skill}</p>
+                    <p><span className="title">Average:</span> {(i.grades.reduce((a, b) => parseInt(a) + parseInt(b))}</p>
                   </div>
-
                   <div className={(i.showGrades ? " " : "hidden") + " grades"}>
                     {i.grades.map((grade, index) => (
                       <p key={index} className="grade"><span className="title" >Grade:</span>  {grade}</p>
                     ))}
                   </div>
+                  <div className="tags">
+                    {i.tags ?
+                      i.tags.map(tag => (
+                        <div className="tag" key={tag}>{tag}</div>
+                      ))
+                      : ''}
+
+                  </div>
+                  <input type="text" className="add-tag" placeholder="Add tag here" onKeyUp={this.addTags.bind(this, index)} />
+
                 </div>
-                <button className="expand-btn" onClick={this.expandGrades.bind(this, index,i)}><i className="fas fa-plus"></i></button>
+                <button className="expand-btn" onClick={this.expandGrades.bind(this, index)}><i className="fas fa-plus"></i></button>
 
               </div>
             </div>
